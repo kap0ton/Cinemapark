cfdd1341fdef3aa5ea7441fa3dfed28f0ab862ad
kap_ton <kap_ton@me.com>
2012-10-13 13:35:34 +0400

    page for muliplex selection added

--- www/js/cinemapark.js
+++ www/js/cinemapark.js
@@ -2,7 +2,6 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -10,7 +9,6 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // `load`, `deviceready`, `offline`, and `online`.
     bindEvents: function() {
-        
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -18,7 +16,6 @@ var app = {
     // The scope of `this` is the event. In order to call the `receivedEvent`
     // function, we must explicity call `app.receivedEvent(...);`
     onDeviceReady: function() {
-        
         app.receivedEvent('deviceready');
         cinemapark.initialize();
     },
@@ -38,38 +35,134 @@ var app = {
 var cinemapark = {
     initialize: function() {
         app.receivedEvent('cinemapark initialized');
-        getCinemas();
+        container = $('#content');
+        this.loadTemplates();
+    },
+    loadTemplates: function(){
+        app.receivedEvent('loading templates...');
+        for(var key in templates){
+            var _key = key.toString();
+            cinemapark.loadBegin(_key);
+        }
+    },
+    loadBegin: function(key){
+        $.get( templates[key], function(templ){
+              app.receivedEvent(key + ' loaded');
+              cinemapark.loadFinish(templ, key);
+              });
+    },
+    loadFinish: function(tmpl, key){
+        app.receivedEvent(key + ' saved');
+        templates[key] = tmpl;
+        if(key == "main")
+            setMainView();
     }
 };
 
-function getCinemas(){
+var container;
+var templates={
+    main: "views/main.html",
+    movie: "views/movie.html",
+    multiplex: "views/multiplex.html"
+};
+
+function resetContainer() {
+    //this removes child elements and cleans up event handlers
+    container.children().remove();
+    container.removeClass("nopadding");
+}
+
+/*--------------------------------------*/
+/*      Main View                       */
+/*--------------------------------------*/
+function setMainView(){
+    app.receivedEvent("set Main View");
+    //container = $("body").find("#content");
+    resetContainer();
+    container.html( templates['main'] );
+    
+    
+    var mName = window.localStorage.getItem('mName');
+    var mId = window.localStorage.getItem('mId');
+    if(mName != null && mId != null) {
+        $('#multLabel').text(mName + ' [' + mId + ']');
+    }
+    
+    $('#selMult').click(function (e) {
+        e.preventDefault();
+        setMultiplexView();
+    });
+    $('#selMov').click(function(e){
+                       e.preventDefault();
+                       setMovieView();
+                       });
+}
+
+/*--------------------------------------*/
+/*      Multiplex View                  */
+/*--------------------------------------*/
+function setMultiplexView(){
+    app.receivedEvent('set Multiplex View');
+    resetContainer();
+    container.html(templates['multiplex']);
+    
+    loadMultiplexes();
+    
+    $('#hrfMainView1').click(function(e){
+                            e.preventDefault();
+                            setMainView();
+                            });
+    $('#hrfMainView2').click(function(e){
+                             e.preventDefault();
+                             setMainView();
+                             });
+}
+
+function loadMultiplexes(){
     $.ajax({
            type: "GET",
            url: "http://gadget.www.cinemapark.ru/data/multiplexes/",
            //datatype: "xml",
-           success: suc,
-           error: err
-    });
+           success: loadMultSuccess,
+           error: loadMultError
+           });
 }
 
-function suc(data){
+function loadMultSuccess(data){
     var items = $('data item', data);
     items.each(function(){
                var a = $('<a>');
-               a.html($(this).attr('title'));
+               a.html($(this).attr('city') + ', ' + $(this).attr('title'));
                a.attr('href','#');
+               a.attr('mId', $(this).attr('id'))
+               a.click(function(item){
+                       window.localStorage.setItem('mId', $(this).attr('mId'));
+                       window.localStorage.setItem('mName', $(this).html());
+                       setMainView();
+                       });
                
                var li = $('<li>');
                li.html(a);
                
-               $('ul').append(li);
-    });
+               $('#multList').append(li);
+               });
 }
 
-function err(){
+function loadMultError(){
     alert('error');
 }
 
-function setMainView(){
+
+/*--------------------------------------*/
+/*      Movie View                       */
+/*--------------------------------------*/
+function setMovieView(){
+    app.receivedEvent('set Movie View');
+    resetContainer();
+    container.html(templates['movie']);
     
+    $('#hrfMainView').click(function(e){
+                            e.preventDefault();
+                            setMainView();
+                            });
 }
